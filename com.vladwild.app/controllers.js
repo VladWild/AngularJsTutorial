1b7e13140812076898e6471d66cecec4218b22cd
VladWild <33370139+VladWild@users.noreply.github.com>
2019-03-17 15:10:16 +0300

    step-7 Разбор простых фильтров и filter

--- com.vladwild.app/controllers.js
+++ com.vladwild.app/controllers.js
@@ -4,18 +4,24 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.title = 'Phones';
     $scope.phones = [
         {
             'name': 'Samsung Galaxy Tab',
-            'snippet': 'The Samsung Galaxy Tab brings you an ultra-mobile entertainment experience through its 7 display'
+            'snippet': 'The Samsung Galaxy Tab brings you an ultra-mobile entertainment experience through its 7 display',
+            'status': true
         },
         {
             'name': 'Samsung Galaxy S phone',
-            'snippet': 'The Samsung Showcase delivers a cinema quality experience like you never seen before.'
+            'snippet': 'The Samsung Showcase delivers a cinema quality experience like you never seen before.',
+            'status': false
         },
         {
             'name': 'DROID 2 Global by Motorola',
-            'snippet': 'The first smartphone with a 1.2 GHz processor and global capabilities.'
+            'snippet': 'The first smartphone with a 1.2 GHz processor and global capabilities.',
+            'status': true
         }
     ];
+
+    $scope.today = new Date();
 });
